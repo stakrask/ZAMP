af2fe1e60b7e9c55d2f8194648da7d007677bb02
stakrask <kraskiewicz.s@gmail.com>
2025-10-20 13:49:53 +0200

    Dodanie zalążka do repozytorium

---
+++ zalazek/src/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <dlfcn.h>
+#include <cassert>
+#include "AbstractInterp4Command.hh"
+
+using namespace std;
+
+
+int main()
+{
+  void *pLibHnd_Move = dlopen("libInterp4Move.so",RTLD_LAZY);
+  AbstractInterp4Command *(*pCreateCmd_Move)(void);
+  void *pFun;
+
+  if (!pLibHnd_Move) {
+    cerr << "!!! Brak biblioteki: Interp4Move.so" << endl;
+    return 1;
+  }
+
+
+  pFun = dlsym(pLibHnd_Move,"CreateCmd");
+  if (!pFun) {
+    cerr << "!!! Nie znaleziono funkcji CreateCmd" << endl;
+    return 1;
+  }
+  pCreateCmd_Move = reinterpret_cast<AbstractInterp4Command* (*)(void)>(pFun);
+
+
+  AbstractInterp4Command *pCmd = pCreateCmd_Move();
+
+  cout << endl;
+  cout << pCmd->GetCmdName() << endl;
+  cout << endl;
+  pCmd->PrintSyntax();
+  cout << endl;
+  pCmd->PrintCmd();
+  cout << endl;
+  
+  delete pCmd;
+
+  dlclose(pLibHnd_Move);
+}
